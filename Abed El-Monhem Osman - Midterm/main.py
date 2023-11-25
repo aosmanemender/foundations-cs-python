aa6f436de0c66720e638975078b44d490fe17849
osman <osman@payleadr.com>
2023-11-25 21:37:49 +0200

    display tabs indexed

--- Abed El-Monhem Osman - Midterm/main.py
+++ Abed El-Monhem Osman - Midterm/main.py
@@ -1,8 +1,8 @@
-# function: greetingUser
-# description: displaying user greeting
 import requests
 from bs4 import BeautifulSoup
 
+# function: greetingUser
+# description: displaying user greeting
 def greetingUser():
   print("\n***** Hello, And Welcome *****\n")
 
@@ -20,6 +20,24 @@ def displayMenu():
   print("7. Save Tabs")
   print("8. Import Tabs")
   print("9. Exit")
+
+# function: openTab
+# params:
+#   tabs: dictionary of tabs to be updated
+# description: adding a new tab to the tabs dict
+def openTab(tabs):
+  print("\n***** Opening a new tab *****")
+
+  while True:
+    tab_title = input("\nEnter the tab title: ")
+    tab_url = input("Enter the tab URL: ")
+
+    if validateAddedTab(tab_title, tab_url):
+      tab_url_dict = {'URL': tab_url, 'Nested Tabs': {}}
+      tabs[tab_title] = tab_url_dict
+      print("\n-> Tab added successfully 👍\n")
+      displayTabsIndexed(tabs)
+      break
 
 # function: validateAddedTab
 # params: 
@@ -39,26 +57,13 @@ def validateAddedTab(tab_title, tab_url):
 
   return valid
 
-# function: openTab
+# function: displayTabsIndexed
 # params:
-#   tabs: dictionary of tabs to be updated
-# description: adding a new tab to the tabs dict
-def openTab(tabs):
-  print("\n***** Opening a new tab *****")
-
-  while True:
-    tab_title = input("\nEnter the tab title: ")
-    tab_url = input("Enter the tab URL: ")
-
-    if validateAddedTab(tab_title, tab_url):
-      tab_url_dict = {
-        'URL' : tab_url,
-        'Nested Tabs' : {}
-      }
-      tabs[tab_title] = tab_url_dict
-      print("\n-> Tab added successfully 👍")
-      displayTabs(tabs)
-      break
+#   tabs: dictionary of tabs to be printed
+# description: displaying all tabs with index number to be picked by the user
+def displayTabsIndexed(tabs):
+  for i, key in enumerate(tabs, 1):
+    print(f"{i}. {key} -> {tabs.get(key).get('URL')}")
 
 # function: closeTab
 # params:
@@ -163,6 +168,7 @@ def importTabs():
 def exit():
   print("bye 👋 bye 👋")
 
+# only for testing purpose
 def initializeTabsDictionary():
   tabs = {}
   tabs["Tab 1"] = "https://www.google.com"
@@ -170,6 +176,65 @@ def initializeTabsDictionary():
   tabs["Tab 3"] = "https://www.facebook.com"
   tabs["Tab 4"] = "https://www.twitter.com"
   tabs["Tab 5"] = "https://www.instagram.com"
+  tabs = {
+    'Tab 1': 'https://www.google.com',
+    'Tab 2': 'https://www.youtube.com',
+    'Tab 3': 'https://www.facebook.com',
+    'Tab 4': 'https://www.twitter.com',
+    'Tab 5': 'https://www.instagram.com'
+  }
+  tabs = {
+    'Tab 1': { 
+        'URL': 'https://www.google.com',
+        'Nested Tabs': { 
+          'Nested tab 1.1': 'https://www.google.com/search?q=python',
+          'Nested tab 1.2': 'https://www.google.com/search?q=java',
+          'Nested tab 1.3': 'https://www.google.com/search?q=c++',
+          'Nested tab 1.4': 'https://www.google.com/search?q=javascript',
+          'Nested tab 1.5': 'https://www.google.com/search?q=php',
+        }
+     },
+    'Tab 2': {
+      'URL': 'https://www.youtube.com',
+      'Nested Tabs': {
+        'Nested tab 2.1': 'https://www.youtube.com/results?search_query=python',
+        'Nested tab 2.2': 'https://www.youtube.com/results?search_query=java',
+        'Nested tab 2.3': 'https://www.youtube.com/results?search_query=c++',
+        'Nested tab 2.4': 'https://www.youtube.com/results?search_query=javascript',
+        'Nested tab 2.5': 'https://www.youtube.com/results?search_query=php',
+      }
+    },
+    'Tab 3': {
+      'URL': 'https://www.facebook.com',
+      'Nested Tabs': {
+        'Nested tab 3.1': 'https://www.facebook.com/search?q=python',
+        'Nested tab 3.2': 'https://www.facebook.com/search?q=java',
+        'Nested tab 3.3': 'https://www.facebook.com/search?q=c++',
+        'Nested tab 3.4': 'https://www.facebook.com/search?q=javascript',
+        'Nested tab 3.5': 'https://www.facebook.com/search?q=php',
+      }
+    },
+    'Tab 4': {
+      'URL': 'https://www.twitter.com',
+      'Nested Tabs': {
+        'Nested tab 4.1': 'https://www.twitter.com/search?q=python',
+        'Nested tab 4.2': 'https://www.twitter.com/search?q=java',
+        'Nested tab 4.3': 'https://www.twitter.com/search?q=c++',
+        'Nested tab 4.4': 'https://www.twitter.com/search?q=javascript',
+        'Nested tab 4.5': 'https://www.twitter.com/search?q=php',
+      }
+    },
+    'Tab 5': {
+      'URL': 'https://www.instagram.com',
+      'Nested Tabs': {
+        'Nested tab 5.1': 'https://www.instagram.com/search?q=python',
+        'Nested tab 5.2': 'https://www.instagram.com/search?q=java',
+        'Nested tab 5.3': 'https://www.instagram.com/search?q=c++',
+        'Nested tab 5.4': 'https://www.instagram.com/search?q=javascript',
+        'Nested tab 5.5': 'https://www.instagram.com/search?q=php',
+      }
+    }
+  }
   return tabs
 
 def menu():
@@ -182,8 +247,7 @@ def menu():
     try:
       choice = int(input("-> "))
       if choice == 1:
-        tabs = openTab(tabs)
-        displayTabs(tabs)
+        openTab(tabs)
       elif choice == 2:
         closeTab(tabs)
       elif choice == 3:
